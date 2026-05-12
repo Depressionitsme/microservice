d7904aa2d854b4b9a4fbfe38e62f3d16803ee38a
Depressionitsme <relombzpt@gmail.com>
2026-05-13 00:03:34 +0200

    all done

---
+++ orders-service/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); 
+
+app.post('/api/orders/process', (req, res) => {
+    console.log("Otrzymano zamówienie:", req.body);
+    res.json({ message: "Zamówienie przetworzone pomyślnie!" });
+});
+
+app.listen(3000);
